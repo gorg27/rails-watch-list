c9dbf88ed42a7be630b847bd4038ab2269579def
Gorgui FAYE <gorgui92@msn.com>
2022-08-19 10:56:47 +0200

    Prepare rails app with external specs

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,17 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+  end
+
+
+end
